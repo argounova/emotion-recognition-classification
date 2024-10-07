8873c1245d54766ebead51a1169fa8c451f58f7f
Craig Putzstuck <putzstuck@gmail.com>
2024-10-07 18:29:06 -0400

    model built...working on improving accuracy

--- models/train/train.py
+++ models/train/train.py
@@ -10,6 +10,98 @@ emotion_ds = tf.keras.utils.image_dataset_from_directory(
   batch_size=32
 )
 
+print(len(emotion_ds))
+
 # Get the class names
 class_names = emotion_ds.class_names
-print(class_names)
+
+# Split the dataset into training, validation, and test datasets
+def get_partitions(
+  ds,
+  train_size=0.8,
+  val_size=0.1,
+  test_size=0.1,
+  shuffle=True,
+  shuffle_size=10000,
+):
+  ds_size=len(ds)
+  if shuffle:
+    ds = ds.shuffle(shuffle_size, seed=12)
+  train_size = int(ds_size * train_size)
+  val_size = int(ds_size * val_size)
+  test_size = int(ds_size * test_size)
+  return ds.take(train_size), ds.skip(train_size).take(val_size), ds.skip(train_size + val_size).take(test_size)
+  
+# Get the training, validation, and test datasets
+train_ds, val_ds, test_ds = get_partitions(emotion_ds)
+
+# Optimize the datasets for performance
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# Data augmentation
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.RandomFlip('horizontal', input_shape=(150, 150, 3)),
+  tf.keras.layers.RandomRotation(0.1),
+  tf.keras.layers.RandomZoom(0.1),
+])
+
+# Build the model
+model = tf.keras.Sequential([
+  data_augmentation,
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Dropout(0.1),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(6, activation='softmax')
+])
+
+# Compile the model
+model.compile(
+  optimizer='adam',
+  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy']
+)
+
+print(model.summary())
+
+# Train the model
+epochs=10
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs,
+  verbose=1,
+  batch_size=32
+)
+
+# Visualize the training results
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
