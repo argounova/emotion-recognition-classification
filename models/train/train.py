1c6853c38dd857209942707356527368a8b13168
Craig Putzstuck <putzstuck@gmail.com>
2024-10-17 19:54:38 -0400

    attempting to improve model accuracy...still hovering around 35%

--- models/train/train.py
+++ models/train/train.py
@@ -4,43 +4,41 @@ import pandas as pd
 import os
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Rescaling, RandomFlip, RandomRotation, RandomZoom, RandomContrast, BatchNormalization
+from keras import layers
+from keras import Sequential
+
 
 
 # Load the dataset
 emotion_ds = 'data/emotions'
-df = pd.DataFrame([name for name in os.listdir(emotion_ds) if name != '.DS_Store'], columns=['Emotion'])
-print(df)
 
 
 # Load the training data
-# train_ds = tf.keras.utils.image_dataset_from_directory(
-#   'data/emotions',
-#   labels='inferred',
-#   label_mode='int',
-#   color_mode='rgb',
-#   batch_size=32,
-#   image_size=(144, 144),
-#   shuffle=True,
-#   seed=123,
-#   validation_split=0.2,
-#   subset='training',
-# )
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  emotion_ds,
+  labels='inferred',
+  label_mode='int',
+  color_mode='rgb',
+  batch_size=32,
+  image_size=(144, 144),
+  shuffle=True,
+  seed=123,
+  validation_split=0.2,
+  subset='training',
+)
 
-# val_ds = tf.keras.utils.image_dataset_from_directory(
-#   'data/emotions',
-#   labels='inferred',
-#   label_mode='int',
-#   color_mode='rgb',
-#   batch_size=32,
-#   image_size=(144, 144),
-#   shuffle=True,
-#   seed=123,
-#   validation_split=0.2,
-#   subset='validation',
-# )
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  emotion_ds,
+  labels='inferred',
+  label_mode='int',
+  color_mode='rgb',
+  batch_size=32,
+  image_size=(144, 144),
+  shuffle=True,
+  seed=123,
+  validation_split=0.2,
+  subset='validation',
+)
 
 # class_labels = train_ds.class_names
 # plt.figure(figsize=(15,10))
@@ -86,79 +84,79 @@ print(df)
 # train_ds, val_ds, test_ds = get_partitions(emotion_ds)
 
 # Optimize the datasets for performance
-# AUTOTUNE = tf.data.AUTOTUNE
-# train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-# val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 # Data augmentation
-# data_augmentation = tf.keras.Sequential([
-#   RandomFlip('horizontal', input_shape=(144, 144, 3)),
-  # RandomRotation(0.1),
-  # RandomZoom(0.1),
-  # RandomContrast(0.1),
-# ])
+data_augmentation = tf.keras.Sequential([
+  layers.RandomFlip('horizontal', input_shape=(144, 144, 3)),
+  layers.RandomRotation(0.1),
+  layers.RandomZoom(0.1),
+  layers.RandomContrast(0.1),
+])
 
 # Build the model
-# model = Sequential([
-#   # data_augmentation,
-#   Rescaling(1./255, input_shape=(144, 144, 3)),
+model = Sequential([
+  data_augmentation,
+  layers.Rescaling(1./255, input_shape=(144, 144, 3)),
 
-#   Conv2D(32, (3,3), activation='relu'),
-#   MaxPooling2D((2,2)),
-#   Dropout(0.2),
+  layers.Conv2D(32, (3,3), activation='relu'),
+  layers.MaxPooling2D((2,2)),
+  layers.Dropout(0.2),
 
-#   Conv2D(64, (3,3), activation='relu'),
-#   MaxPooling2D((2,2)),
-#   Dropout(0.3),
+  layers.Conv2D(64, (3,3), activation='relu'),
+  layers.MaxPooling2D((2,2)),
+  layers.Dropout(0.3),
   
-#   Conv2D(128, (3,3), activation='relu'),
-#   MaxPooling2D((2,2)),
-#   Dropout(0.4),
+  layers.Conv2D(128, (3,3), activation='relu'),
+  layers.MaxPooling2D((2,2)),
+  layers.Dropout(0.4),
 
-#   Flatten(),
-#   Dense(128, activation='relu'),
-#   Dropout(0.5),
-#   Dense(6, activation='softmax')
-# ])
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dropout(0.5),
+  layers.Dense(6, activation='softmax')
+])
 
 # Compile the model
-# model.compile(
-#   optimizer='adam',
-#   loss='sparse_categorical_crossentropy',
-#   metrics=['accuracy']
-# )
+model.compile(
+  optimizer='adam',
+  loss='sparse_categorical_crossentropy',
+  metrics=['accuracy']
+)
 
-# print(model.summary())
+print(model.summary())
 
 # # Train the model
-# epochs=5
-# history = model.fit(
-#   train_ds,
-#   validation_data=val_ds,
-#   epochs=epochs,
-#   verbose=1,
-#   batch_size=32
-# )
+epochs=5
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs,
+  verbose=1,
+  batch_size=32
+)
 
 # Visualize the training results
-# acc = history.history['accuracy']
-# val_acc = history.history['val_accuracy']
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-# epochs_range = range(epochs)
+epochs_range = range(epochs)
 
-# plt.figure(figsize=(8, 8))
-# plt.subplot(1, 2, 1)
-# plt.plot(epochs_range, acc, label='Training Accuracy')
-# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-# plt.legend(loc='lower right')
-# plt.title('Training and Validation Accuracy')
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
 
-# plt.subplot(1, 2, 2)
-# plt.plot(epochs_range, loss, label='Training Loss')
-# plt.plot(epochs_range, val_loss, label='Validation Loss')
-# plt.legend(loc='upper right')
-# plt.title('Training and Validation Loss')
-# plt.show()
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
